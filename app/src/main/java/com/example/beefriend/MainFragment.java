e954d6d04db361dc9a8f6b3eeeb98f78219d9e73
zumaker <bee_panupong@hotmail.com>
2019-02-23 15:56:46 +0700

    After Break lunch

--- app/src/main/java/com/example/beefriend/MainFragment.java
+++ app/src/main/java/com/example/beefriend/MainFragment.java
@@ -2,28 +2,50 @@ package com.example.beefriend;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment
+{
 
 
-    public MainFragment() {
+    public MainFragment()
+    {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+//        Register Controller
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+//                Replace Fragment
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentMainFragment,new RegisterFragment()).addToBackStack(null).commit();
+            }
+        });
+
+    } // Main Method
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
-}
+} // Main Class
--- app/src/main/java/com/example/beefriend/RegisterFragment.java
+++ app/src/main/java/com/example/beefriend/RegisterFragment.java
@@ -2,7 +2,9 @@ package com.example.beefriend;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +13,36 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RegisterFragment extends Fragment {
+public class RegisterFragment extends Fragment
+{
 
 
-    public RegisterFragment() {
+    public RegisterFragment()
+    {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+    } //Main Method
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
