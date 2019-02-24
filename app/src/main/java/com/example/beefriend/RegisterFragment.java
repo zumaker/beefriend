ea661a704f11ed10a11e855aff2511859b828e34
zumaker <bee_panupong@hotmail.com>
2019-02-24 12:04:34 +0700

    lunch day2

--- app/src/main/java/com/example/beefriend/RegisterFragment.java
+++ app/src/main/java/com/example/beefriend/RegisterFragment.java
@@ -1,48 +1,153 @@
 package com.example.beefriend;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RegisterFragment extends Fragment
-{
+public class RegisterFragment extends Fragment {
+    private boolean aBoolean = true;
+    private  ImageView imageView ;
+    private Uri uri ;
 
 
-    public RegisterFragment()
-    {
+
+
+    public RegisterFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState)
-    {
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
 //        Create Toolbar
-        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
-        ((MainActivity)getActivity()).getSupportActionBar().setTitle("Register");
-        ((MainActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
-        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener()
+        createToolbar();
+        //        Choose ImageView
+        chooseImage();
+
+    } //Main Method
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == getActivity().RESULT_OK)
         {
-            @Override
-            public void onClick(View v)
+            uri = data.getData();
+            aBoolean = false;
+
+            try
             {
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap,800,600,false);
+                imageView.setImageBitmap(bitmap1);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void chooseImage()
+    {
+        imageView = getView().findViewById(R.id.imvAvata);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Choose App"), 1);
+
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.itemUpload) {
+            checkValue();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void checkValue() {
+        MyAlert myAlert = new MyAlert(getActivity());
+
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+        String name = nameEditText.getText().toString().trim() ;
+        String user = userEditText.getText().toString().trim() ;
+        String password = passwordEditText.getText().toString().trim() ;
+
+
+        if (aBoolean)
+        {
+//           Non Choose Image
+            myAlert.normalDialog("Non Choose Image", "Please Choose Avata");
+        }
+        else if (name.isEmpty() || user.isEmpty() || password.isEmpty())
+        {
+//            Have Space
+            myAlert.normalDialog("No DATA","Please input Data");
+        }
+        else
+        {
+            
+        }
+
+
+
+
+
+
+    }//checkValue
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_register, menu);
+
+    }
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
 
-    } //Main Method
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
